adcb0b40ccba50b8fbed9bf9cb2e8f399d081880
shivramsingh13 <91517543+shivramsingh13@users.noreply.github.com>
2023-05-16 09:31:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,3 +34,52 @@ resource "azurerm_public_ip" "public_ip" {
   }
 }
 
+resource "azurerm_virtual_machine" "vm-1" {
+ name = var.vm_name
+ resource_grou_name = azurerm_resource_group.rg-1.name
+ location = azurerm_resource_group.rg-1.location
+ vm_size = "Standard_F2"
+ network_interface_ides = [azurerm_network_interface.nic-1.id]
+ os_profile {
+  computer_name = "hostname"
+  admin_username = "adminuser"
+  admin_password = "shiv@1234567"
+  }
+  
+  os_profile_windows_config {
+   }
+   
+   Storage_os_disk {
+    name = var.os_disk_name
+    caching = "ReadWrite"
+    create_option = "FromImage"
+    managed_disk_type = "Standard_LRS"
+    }
+   storage_image_reference {
+     publisher = "MicrosoftWindowsServer"
+     offer = "WindowsServer"
+     sku = "2016-Datacenter"
+     version = "latest"
+     }
+   }
+   
+   resource "azurerm_managed_disk" "disk-2" {
+     name = var.data_disk_name
+     resource_group_name = azurerm_resource_group.rg-1.name
+     location = azurerm_resource_group.rg-1.location
+     storage_account_type = "Standard_LRS"
+     create_option = "empty"
+     disk_size_gb = "30"
+    }
+    
+    resource "azurerm_virtual_machine_data_attachment" "disk-2" {
+     managed_disk_id = azurerm_managed_disk.disk-2.id
+     virtual_machine_id = zurerm_virtual_machine.vm-1.id
+     lun = "10"
+     caching = "ReadWrite"
+    }
+    
+     
+     
+  
+
